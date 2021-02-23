b153af080867bc6a6a5e3c09a47d2717eb99ebf3
muhrizqi <rizqi@arkana.co.id>
2021-02-23 17:04:20 +0700

    add button using owl

---
+++ pos_demo/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "POS demo",
+    'summary': "Point of sale demo module",
+    'description': """Long description""",
+    'author': "Muhammad Rizqi",
+    'website': "http://www.example.com",
+    'category': 'Point of Sale',
+    'version': '14.0.1',
+    'depends': ['point_of_sale'],
+    'data': [
+    ],
+    'qweb': [
+        'static/src/xml/pos_demo.xml'
+    ]
+}
